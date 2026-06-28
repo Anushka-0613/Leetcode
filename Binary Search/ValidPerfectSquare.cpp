67c9569bd7b292e9573e71fb798c59047a1ff842
ANUSHKA AGARWAL <2024uce0043@iitjammu.ac.in>
2026-06-29 00:02:05 +0530

    Solved Calid Perfect Square

---
+++ Binary Search/ValidPerfectSquare.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 0)
+            return true;
+
+        int left = 1;
+        int right = num;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (mid > num / mid) {
+                right = mid - 1;
+            }
+            else if (mid < num / mid) {
+                left = mid + 1;
+            }
+            else {
+                if(num % mid ==0){
+                    return true;
+                }
+                else 
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+};
