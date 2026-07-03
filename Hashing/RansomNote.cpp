4af7f74106e8d2525885d23cf595cbc3d793ee9a
ANUSHKA AGARWAL <2024uce0043@iitjammu.ac.in>
2026-07-03 09:49:27 +0530

    Solved Ransom Note

---
+++ Hashing/RansomNote.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map <char,int> mp;
+
+        for(int i=0; i<ransomNote.size(); i++){
+            mp[ransomNote[i]]++;
+        }
+        for(int i=0; i<magazine.size(); i++){
+            mp[magazine[i]]--;
+        }
+        for(auto it : mp){
+            if(it.second > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
